7c5150577cb1be1a7f838ad0a0ae34e3444c9362
nikung007 <105716681+nikung007@users.noreply.github.com>
2022-06-15 12:07:07 +0530

    update 12-06
    Afternoon

---
+++ src/features/coin/Coin.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Coin() {
+  return (
+    <div className='btn'>
+
+        <span>Coin : 0</span>
+
+    </div>
+  )
+}
+
+export default Coin
---
+++ src/features/counter/Counter.js
@@ -0,0 +1,17 @@
+function Counter() {
+  return (
+    <div>
+        <div className='btn'>
+
+            <button onClick={ () => { }}>Increment</button>
+
+            <span>Count : 0</span>
+
+            <button onClick={ () => { }}>Decrement</button>
+
+        </div>
+    </div>
+  )
+}
+
+export default Counter;
